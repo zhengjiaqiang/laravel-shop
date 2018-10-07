c6bf1cd3fcdde11371b402e4325686492fa181b0
zhengjiaqiang <2719956702@qq.com>
2018-10-07 14:37:56 +0800

    邮箱验证中间件

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -10,4 +10,10 @@ class PagesController extends Controller
     {
         return view('pages.root');
     }
+
+    //邮箱验证
+    public function emailVerifyNotice(Request $request)
+    {
+        return view('pages.email_verify_notice');
+    }
 }
